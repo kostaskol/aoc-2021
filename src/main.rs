ccb7e85b1f2c191de877a62cccc4d66d83fb04ac
Kostas Kolivas <kkolivas@skroutz.gr>
2021-12-02 18:36:08 +0200

    Solve P1 (2 star)

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+extern crate clap;
+use clap::{Arg, App};
+
+mod utils;
+mod p1;
+
+fn main() {
+    let matches = App::new("Advent of code!")
+        .arg(Arg::new("extra")
+                .short('e')
+                .long("extra")
+                .takes_value(false))
+        .arg(Arg::new("problem_number")
+                .short('p')
+                .long("problem")
+                .takes_value(true))
+        .get_matches();
+
+    let extra_star = matches.is_present("extra");
+    let problem = matches.value_of("problem_number").unwrap_or("1");
+
+    match problem {
+        "1" => p1::run(extra_star),
+        &_ => println!("Only know how to solve #{:?} for now :(", 1)
+    }
+}
